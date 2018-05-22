1f4082620322d9ba2541f8ef57cb84dd6a2f5848
alekseimi <milosevic.aleksej@gmail.com>
2018-05-22 16:40:27 +0200

    Ustvarjeni REST providerji in hitri mockupi seznamov

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,44 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import {HttpClientModule} from "@angular/common/http";
+import { RestClan } from '../providers/rest-clan/rest-clan';
+import { RestPorociloProvider } from '../providers/rest-porocilo/rest-porocilo';
+import { RestAktivnostProvider } from '../providers/rest-aktivnost/rest-aktivnost';
+import {SeznamAktivnostiPage} from "../pages/seznam-aktivnosti/seznam-aktivnosti";
+import {DetailsAktivnostiPage} from "../pages/details-aktivnosti/details-aktivnosti";
+import {SeznamPorocilPage} from "../pages/seznam-porocil/seznam-porocil";
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SeznamPorocilPage,
+    SeznamAktivnostiPage,
+    DetailsAktivnostiPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SeznamPorocilPage,
+    SeznamAktivnostiPage,
+    DetailsAktivnostiPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RestClan,
+    RestPorociloProvider,
+    RestAktivnostProvider
   ]
 })
 export class AppModule {}
---
+++ src/model/clan.ts
@@ -0,0 +1,18 @@
+import {Porocilo} from "./porocilo";
+
+export class Clan {
+  id?: number;
+  uporabnisko_ime: string;
+  hash_geslo: string;
+  elektronska_posta: string;
+  ime: string;
+  priimek: string;
+  vloga: string;
+  telefonska_stevilka: string;
+  porocila: Porocilo[];
+
+  constructor(){
+
+  }
+
+}
