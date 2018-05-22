f658239c4a591315a3ef2938ce382d4f86b2074f
alekseimi <milosevic.aleksej@gmail.com>
2018-05-23 01:08:00 +0200

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,13 +18,13 @@ import { RestAktivnostProvider } from '../providers/rest-aktivnost/rest-aktivnos
 import {SeznamAktivnostiPage} from "../pages/seznam-aktivnosti/seznam-aktivnosti";
 import {DetailsAktivnostiPage} from "../pages/details-aktivnosti/details-aktivnosti";
 import {SeznamPorocilPage} from "../pages/seznam-porocil/seznam-porocil";
+import {MapPage} from "../pages/map/map";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     MapPage,
-    ListPage,
     SeznamPorocilPage,
     SeznamAktivnostiPage,
     DetailsAktivnostiPage
@@ -41,7 +40,6 @@ import {SeznamPorocilPage} from "../pages/seznam-porocil/seznam-porocil";
     MyApp,
     HomePage,
     MapPage,
-    ListPage,
     SeznamPorocilPage,
     SeznamAktivnostiPage,
     DetailsAktivnostiPage
---
+++ src/pages/registration/registration.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RegistrationPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-registration',
+  templateUrl: 'registration.html',
+})
+export class RegistrationPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+//TEST ALI DELA
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegistrationPage');
+  }
+
+}
